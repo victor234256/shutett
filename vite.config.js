b25febc00e928ba337aaa1903ad2e65187f2e0f1
victor234256 <charity.jv.jvo@gmail.com>
2024-08-18 14:18:05 -0700

    debug vercel file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,8 +10,5 @@ export default defineConfig({
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
   },
 });
