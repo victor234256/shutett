0cefeb433446aa58c7a92bc120989b736cc94907
victor234256 <charity.jv.jvo@gmail.com>
2024-08-18 14:19:41 -0700

    debug vercel file

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./App";
-import Redirect from "./redirectLinks";
+import App from "./App.jsx";
+import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/:shorturl" element={<Redirect />} />
-    </Routes>
-  </Router>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
